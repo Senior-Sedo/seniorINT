d5c7fab1693f8bc97026d57e614d49ae0f4e4a92
Senior Sedo <145229171+Senior-Sedo@users.noreply.github.com>
2024-11-27 18:47:51 +0100

    Minified /dist + Added JSDoc + CDN Support

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,35 @@
-const seniorINT = (min: number, max: number): number => Math.floor(Math.random() * (max - min + 1) + min);
+/**
+ * Generates a random integer number between min and max.
+ * @param {number} min - required
+ * @param {number} max - required
+ * @returns {number} number
+ * @throws TypeError if either min or max is NaN
+ *
+ * @example
+ * import seniorINT from "@ssts/int";
+ * const randomInteger: number = seniorINT(-100, 100);
+ *
+ * console.log(randomInteger);
+ * // Log example: 74
+ */
+const seniorINT = (min: number, max: number): number => {
+  if (typeof min !== "number") throw new TypeError("min must be a number");
+  if (typeof max !== "number") throw new TypeError("max must be a number");
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+declare global {
+  interface Window {
+    ssts: {
+      seniorINT?: typeof seniorINT;
+    };
+  }
+}
+
+if (window != undefined) {
+  window.ssts = {};
+  window.ssts.seniorINT = seniorINT;
+}
+
 export default seniorINT;
